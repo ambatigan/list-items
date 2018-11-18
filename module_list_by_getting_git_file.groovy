bb6b5c97f90059ef430dcb494ea8877110b38de3
ambatigan <gangadhar.ambati@saven.in>
2018-11-18 21:13:20 +0530

    Using different commenting style for groovy..

--- module_list_by_getting_git_file.groovy
+++ module_list_by_getting_git_file.groovy
@@ -1,7 +1,8 @@
 def command = '''
 //for the aws codecommit repo
 //aws codecommit get-file --repository-name Testing-Automation --file-path /Jenkins/Dev/Resources/Input_data/tag_names.txt|jq -r '.fileContent'|base64 -d > /tmp/tag_names_out.txt
-//for the github repo
+/* for the github repo
+   Using different commenting style */
 svn export https://github.com/ambatigan/list-items.git/trunk/items_list.txt --force && cp items_list.txt /tmp/tag_names_out.txt
 '''
 def proc = ['bash', '-c', command].execute()
