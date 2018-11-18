343ce71225003c748a3c57457e1db63de84c10c3
ambatigan <gangadhar.ambati@saven.in>
2018-11-18 21:06:04 +0530

    adding the github file implementation.. here using svn export for checking out the github file..

--- module_list_by_getting_git_file.groovy
+++ module_list_by_getting_git_file.groovy
@@ -1,5 +1,8 @@
 def command = '''
-aws codecommit get-file --repository-name Testing-Automation --file-path /Jenkins/Dev/Resources/Input_data/tag_names.txt|jq -r '.fileContent'|base64 -d > /tmp/tag_names_out.txt
+//for the aws codecommit repo
+//aws codecommit get-file --repository-name Testing-Automation --file-path /Jenkins/Dev/Resources/Input_data/tag_names.txt|jq -r '.fileContent'|base64 -d > /tmp/tag_names_out.txt
+//for the github repo
+svn export https://github.com/ambatigan/list-items.git/trunk/items_list.txt --force && cp items_list.txt /tmp/tag_names_out.txt
 '''
 def proc = ['bash', '-c', command].execute()
 proc.waitForOrKill(5000)
@@ -13,4 +16,5 @@ stream.close()
 def output =props.keySet() as String[];
 output= output.sort { it.size() }
 def var = Arrays.asList(output)
+System.out.println(var)
 return var
