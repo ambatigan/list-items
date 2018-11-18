8337ae5a3ded291dc9c8b47b12a418adb145f94a
ambatigan <gangadhar.ambati@saven.in>
2018-11-18 09:03:20 +0530

    initial import of the groovy script and items list file...

---
+++ module_list.groovy
@@ -0,0 +1,11 @@
+def props = new Properties()
+def stream = new FileInputStream("items_list.txt")
+try {
+props.load(stream)
+} finally {
+stream.close()
+}
+def parameters=props.keySet() as String[];
+parameters= parameters.sort { it.size() }
+def var = Arrays.asList(parameters)
+return var
